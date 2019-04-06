6b5d48fe1653db2e2974719b21302a8731c6755f
Matt Cieslak <mattcieslak@gmail.com>
2019-04-05 21:36:41 -0400

    add a local test

--- local_test.sh
+++ local_test.sh
@@ -1,1 +1,1 @@
-python fwutil_job_run_local.py ${APIKEY} 5ca77861f546b60028ef120a /Users/mcieslak/projects/upenn/flywheel/qsiprep_fw_debug
+python fwutil_job_run_local.py ${APIKEY} 5ca7a467f546b60027ef49a3 /Users/mcieslak/projects/upenn/flywheel/qsiprep_fw_debug
