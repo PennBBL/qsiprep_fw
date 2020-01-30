1b1a183b887148dea087d510b39456b2c7629ed2
Matt Cieslak <mattcieslak@gmail.com>
2020-01-30 11:32:24 -0500

    update qsiprep to 0.7.1

---
+++ build_gcp.sh
@@ -0,0 +1,4 @@
+# After merging master, run this script to build and upload the hpc-version
+IMAGENAME=$(cat manifest.json | grep \"image\": | sed 's/^.*"image": "\(.*\)".*/\1/')
+docker build -t ${IMAGENAME} .
+fw gear upload
